697618706a0deee619c85bc18a9791a35b8c4a4c
JP <jpc.araujo@hotmail.com>
2021-02-14 23:36:38 -0300

    add page layouts

---
+++ src/pages/_layouts/Auth/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Wrapper } from "./styles.js";
+
+export default function AuthLayout({ children }) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+AuthLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
