e400a909771b15176bcd62e783216259beafe666
JP <jpc.araujo@hotmail.com>
2021-02-14 23:42:37 -0300

    add globalStyle

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Routes from "./routes";
 import { Router } from "react-router-dom";
 import history from "./services/history";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyle />
     </Router>
   );
 }
